e4b77f78a4ae6d13833b6a9da47f46b9c3409e0b
Yang Nie <flytocc@outlook.com>
2025-07-30 09:59:12 +0800

    remove `filter_cfg`

--- configs/rtdetr/rtdetr_r50vd_8xb2-72e_coco.py
+++ configs/rtdetr/rtdetr_r50vd_8xb2-72e_coco.py
@@ -146,7 +146,7 @@ train_dataloader = dict(
     batch_sampler=None,
     drop_last=True ,
     pin_memory=True,
-    dataset=dict(pipeline=train_pipeline))
+    dataset=dict(filter_cfg=None, pipeline=train_pipeline))
 val_dataloader = dict(dataset=dict(pipeline=test_pipeline))
 test_dataloader = dict(dataset=dict(pipeline=test_pipeline))
 
