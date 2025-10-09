b53f624f77372ca86c4abccc7e4c78b84b6fb053
Yang Nie <flytocc@outlook.com>
2025-10-09 13:52:57 +0800

    update rtdetrv2 train_pipeline

--- configs/rtdetrv2/rtdetrv2_r50vd_8xb2-72e_coco.py
+++ configs/rtdetrv2/rtdetrv2_r50vd_8xb2-72e_coco.py
@@ -1,16 +1,36 @@
 _base_ = '../rtdetr/rtdetr_r50vd_8xb2-72e_coco.py'
 
-# optimizer
-optim_wrapper = dict(
-    paramwise_cfg=dict(
-        custom_keys={'in_proj_bias': dict(decay_mult=1.0)}, bias_decay_mult=1.0))
-
 data_preprocessor_stage2 = dict(
     type='DetDataPreprocessor',
     mean=[0, 0, 0],
     std=[255, 255, 255],
     bgr_to_rgb=True,
     pad_size_divisor=1)
+
+train_pipeline = [
+    dict(type='LoadImageFromFile', backend_args={{_base_.backend_args}}),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='RandomApply',
+        transforms=dict(
+            type='PhotoMetricDistortion',
+            hue_delta=12.75,
+            swap_channel=False,
+            clip_val=255,
+            force_float32=False),
+        prob=0.8),
+    dict(type='Expand', mean=[0, 0, 0]),
+    dict(
+        type='RandomApply',
+        transforms=dict(
+            type='MinIoURandomCrop', cover_all_box=False, trials=40),
+        prob=0.8),
+    dict(type='FilterAnnotations', min_gt_bbox_wh=(1, 1), keep_empty=False),
+    dict(type='Resize', scale=(640, 640), keep_ratio=False),
+    dict(type='FilterAnnotations', min_gt_bbox_wh=(1, 1), keep_empty=False),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PackDetInputs')
+]
 
 train_pipeline_stage2 = [
     dict(type='LoadImageFromFile', backend_args={{_base_.backend_args}}),
@@ -21,6 +41,20 @@ train_pipeline_stage2 = [
     dict(type='RandomFlip', prob=0.5),
     dict(type='PackDetInputs')
 ]
+
+test_pipeline = [
+    dict(type='LoadImageFromFile', backend_args={{_base_.backend_args}}),
+    dict(type='Resize', scale=(640, 640), keep_ratio=False),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='PackDetInputs',
+        meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape',
+                   'scale_factor'))
+]
+
+train_dataloader = dict(dataset=dict(pipeline=train_pipeline))
+val_dataloader = dict(dataset=dict(pipeline=test_pipeline))
+test_dataloader = dict(dataset=dict(pipeline=test_pipeline))
 
 stage2_num_epochs = 1
 custom_hooks = [
