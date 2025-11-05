9ef40d0d88e707a324bf9db76b1c045688b7b730
Yang Nie <flytocc@outlook.com>
2025-11-05 15:27:09 +0800

    fix `CheckpointAfterValHook`

--- mmdet/engine/hooks/checkpoint_after_val_hook.py
+++ mmdet/engine/hooks/checkpoint_after_val_hook.py
@@ -11,7 +11,5 @@ class CheckpointAfterValHook(CheckpointHook):
         return
 
     def after_val_epoch(self, runner, metrics):
-        runner.epoch -= 1
         super().after_train_epoch(runner)
-        runner.epoch += 1
         super().after_val_epoch(runner, metrics)
