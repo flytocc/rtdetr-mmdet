a26efa8585c2802b0d81c6ea66e7701c3c0cbfb8
Yang Nie <flytocc@outlook.com>
2025-11-06 14:34:50 +0800

    fix `EMADynamicMomentumHook` and `CheckpointAfterValHook`

--- mmdet/engine/hooks/checkpoint_after_val_hook.py
+++ mmdet/engine/hooks/checkpoint_after_val_hook.py
@@ -11,5 +11,18 @@ class CheckpointAfterValHook(CheckpointHook):
         return
 
     def after_val_epoch(self, runner, metrics):
-        super().after_train_epoch(runner)
+        assert self.by_epoch, \
+            'Only support `by_epoch=True` in CheckpointAfterValHook.'
+
+        # save checkpoint for following cases:
+        # 1. every ``self.interval`` epochs which start at ``self.save_begin``
+        # 2. reach the last epoch of training
+        if self.every_n_epochs(runner, self.interval, self.save_begin +
+                               1) or (self.save_last
+                                      and self.is_last_train_epoch(runner)):
+            runner.logger.info(f'Saving checkpoint at {runner.epoch} epochs')
+            step = runner.epoch
+            meta = dict(epoch=step, iter=runner.iter)
+            self._save_checkpoint_with_step(runner, step, meta=meta)
+
         super().after_val_epoch(runner, metrics)
--- mmdet/engine/hooks/ema_dynamic_momentum_hook.py
+++ mmdet/engine/hooks/ema_dynamic_momentum_hook.py
@@ -2,16 +2,44 @@
 import os
 from typing import Dict, Optional
 
-from mmengine.device import get_device
 from mmengine.hooks.ema_hook import EMAHook
 from mmengine.registry import HOOKS
-from mmengine.runner import Runner
-
+from mmengine.runner import Runner, load_checkpoint
 from mmdet.utils.benchmark import print_log
 
 
 @HOOKS.register_module()
 class EMADynamicMomentumHook(EMAHook):
+    """EMADynamicMomentumHook. This hook implements a two-stage training
+    strategy with dynamic EMA momentum adjustment based on validation
+    performance.
+
+    Args:
+        restart_epoch (int): The epoch to restart training from the best model
+            found in stage 1.
+        restart_momentum (float, optional): The momentum to be set at the
+            beginning of stage 2. If None, keep the momentum unchanged.
+            Defaults to None.
+        interval (float): The interval to increase EMA momentum when no
+            improvement is observed in stage 2. Defaults to 0.0001.
+        metric (str): The metric to monitor. Defaults to 'auto'.
+
+    Note:
+        This hook requires `CheckpointAfterValHook` to be also registered to
+        the runner, and `CheckpointAfterValHook.after_val_epoch` must be called
+        after `EMADynamicMomentumHook.after_val_epoch`.
+
+        1. In stage 1, `after_val_epoch` records the best model based on the
+        specified metric, `before_train_epoch` does nothing.
+        2. At the beginning of stage 2, `before_train_epoch` resumes training
+        from the best model found in stage 1 and resets the EMA momentum to
+        `restart_momentum` if provided, `after_val_epoch` initializes the best
+        metric for stage 2.
+        3. During stage 2, if no improvement is observed in the specified
+        metric compared to the best metric found in stage 2, the hook resumes
+        training from the best model found in stage 1 and increases the EMA
+        momentum by `interval`.
+    """
 
     def __init__(self,
                  *args,
@@ -25,101 +53,211 @@ class EMADynamicMomentumHook(EMAHook):
         self.restart_momentum = restart_momentum
         self.interval = interval
         self.metric = metric
-        self.best_epoch: Optional[int] = None
+        self.cur_metric: Optional[float] = None
+        self.best_epoch_stage1: Optional[int] = None
+        self.best_metric_stage1: Optional[float] = None
+        self.best_metric_stage2: Optional[float] = None
+        self.best_epoch: Optional[float] = None
         self.best_metric: Optional[float] = None
-        self.prev_metric: Optional[float] = None
-        self.cur_momentum: Optional[float] = None
 
     def _resume_best_checkpoint(self, runner: Runner):
-        # resume model and optimizer
-        assert self.best_epoch is not None
+        assert self.best_epoch_stage1 is not None
         filename = os.path.join(runner.work_dir,
-                                f'epoch_{self.best_epoch}.pth')
-        checkpoint = runner.load_checkpoint(
-            filename, map_location=get_device())
+                                f'epoch_{self.best_epoch_stage1}.pth')
+        # model
+        checkpoint = load_checkpoint(
+            runner.model, filename, map_location='cpu', strict=True)
+        # optim_wrapper
         runner.optim_wrapper.load_state_dict(checkpoint['optimizer'])
+        # ema
+        self._load_ema_dynamic_momentum_dict(runner, checkpoint)
+        assert 'ema_state_dict' in checkpoint
+        # The original model parameters are actually saved in ema
+        # field swap the weights back to resume ema state.
+        self._swap_ema_state_dict(checkpoint)
+        self.ema_model.load_state_dict(
+            checkpoint['ema_state_dict'], strict=self.strict_load)
 
-    # TODO some bug to fix
+    def before_train_epoch(self, runner: Runner) -> None:
+        """
+        Note:
+            runner.epoch starts from 0.
+        """
+        super().before_train_epoch(runner)
 
-    # def after_val_epoch(self,
-    #                     runner: Runner,
-    #                     metrics: Optional[Dict[str, float]] = None) -> None:
-    #     super().after_val_epoch(runner, metrics)
+        # stage 1
+        if runner.epoch < self.restart_epoch:
+            return
 
-    #     if self.metric == 'auto':
-    #         metric = metrics[list(metrics.keys())[0]]
-    #     else:
-    #         metric = metrics[self.metric]
+        old_momentum = self.ema_model.momentum
 
-    #     if self.best_metric is None or self.best_epoch is None:
-    #         self.best_metric = metric
-    #         self.best_epoch = runner.epoch
+        # at the beginning of stage 2
+        # resume best checkpoint and reset momentum
+        if runner.epoch == self.restart_epoch:
+            if runner.epoch != self.best_epoch_stage1:
+                print_log(
+                    f'Refresh training from the best model: '
+                    f'epoch_{self.best_epoch_stage1}.pth',
+                    logger=runner.logger)
+                self._resume_best_checkpoint(runner)
+            if self.restart_momentum is not None and \
+                    self.ema_model.momentum != self.restart_momentum:
+                print_log(
+                    f'Change EMA momentum from {old_momentum} '
+                    f'to {self.restart_momentum}',
+                    logger=runner.logger)
+                self.ema_model.momentum = self.restart_momentum
 
-    #     # stage 1
-    #     if runner.epoch <= self.restart_epoch:
-    #         if metric >= self.best_metric:
-    #             self.best_metric = metric
-    #             self.best_epoch = runner.epoch
-    #         elif runner.epoch == self.restart_epoch:
-    #             self._resume_best_checkpoint(runner)
-    #             if self.restart_momentum is not None:
-    #                 self.ema_model.momentum = self.restart_momentum
-    #             print_log(
-    #                 f'Resume training from the best model: '
-    #                 f'epoch_{self.best_epoch}.pth',
-    #                 logger=runner.logger)
+        # after the beginning of stage 2
+        else:
+            if self.cur_metric < self.best_metric_stage2:
+                print_log(
+                    f'Refresh training from the best model: '
+                    f'epoch_{self.best_epoch_stage1}.pth',
+                    logger=runner.logger)
+                self._resume_best_checkpoint(runner)
 
-    #     # stage 2
-    #     else:
-    #         if self.prev_metric is None:
-    #             self.prev_metric = metric
+                print_log(
+                    f'Change EMA momentum from {old_momentum} '
+                    f'to {old_momentum + self.interval}',
+                    logger=runner.logger)
+                self.ema_model.momentum = old_momentum + self.interval
 
-    #         if metric < max(self.prev_metric, self.best_metric):
-    #             self.cur_momentum = self.ema_model.momentum
-    #             self._resume_best_checkpoint(runner)
-    #             self.ema_model.momentum = self.cur_momentum + self.interval
-    #             print_log(
-    #                 f'Change EMA momentum to {self.ema_model.momentum}, '
-    #                 'and resume training from the best model: '
-    #                 f'epoch_{self.best_epoch}.pth',
-    #                 logger=runner.logger)
+    def after_val_epoch(self,
+                        runner: Runner,
+                        metrics: Optional[Dict[str, float]] = None) -> None:
+        """
+        Note:
+            1. runner.epoch starts from 1.
+            2. CheckpointAfterValHook.after_val_epoch must be called after here
+        """
 
-    #             self.prev_metric = self.best_metric
-    #         else:
-    #             self.prev_metric = metric
+        super().after_val_epoch(runner, metrics)
 
-    # def before_save_checkpoint(self, runner, checkpoint: dict) -> None:
-    #     checkpoint['ema_dynamic_momentum_dict'] = {
-    #         'restart_epoch': self.restart_epoch,
-    #         'interval': self.interval,
-    #         'metric': self.metric,
-    #         'cur_momentum': self.cur_momentum or self.ema_model.momentum,
-    #         'best_epoch': self.best_epoch,
-    #         'best_metric': self.best_metric,
-    #         'prev_metric': self.prev_metric
-    #     }
-    #     super().before_save_checkpoint(runner, checkpoint)
+        metric = list(metrics.keys())[0] \
+            if self.metric == 'auto' else self.metric
+        self.cur_metric = metrics[metric]
 
-    # def after_load_checkpoint(self, runner, checkpoint: dict) -> None:
-    #     if 'ema_dynamic_momentum_dict' in checkpoint and runner._resume:
-    #         dynamic_momentum_dict = checkpoint['ema_dynamic_momentum_dict']
-    #         self.restart_epoch = dynamic_momentum_dict['restart_epoch']
-    #         self.interval = dynamic_momentum_dict['interval']
-    #         self.metric = dynamic_momentum_dict['metric']
-    #         self.cur_momentum = dynamic_momentum_dict['cur_momentum']
-    #         self.best_epoch = dynamic_momentum_dict['best_epoch']
-    #         self.best_metric = dynamic_momentum_dict['best_metric']
-    #         self.prev_metric = dynamic_momentum_dict['prev_metric']
-    #         print_log(
-    #             f'Resuming EMA dynamic momentum with: '
-    #             f'restart_epoch={self.restart_epoch}, '
-    #             f'interval={self.interval}, metric={self.metric}, '
-    #             f'cur_momentum={self.cur_momentum}, '
-    #             f'best_epoch={self.best_epoch}, '
-    #             f'best_metric={self.best_metric}, '
-    #             f'prev_metric={self.prev_metric}',
-    #             logger=runner.logger)
+        if self.best_metric is None or self.cur_metric >= self.best_metric:
+            self.best_metric = self.cur_metric
+            self.best_epoch = runner.epoch
 
-    #         self.ema_model.momentum = self.cur_momentum
+        # stage 1
+        if runner.epoch <= self.restart_epoch:
+            if self.best_metric_stage1 is None or \
+                    self.cur_metric >= self.best_metric_stage1:
+                self.best_metric_stage1 = self.cur_metric
+                self.best_epoch_stage1 = runner.epoch
 
-    #     super().after_load_checkpoint(runner, checkpoint)
+        # stage 2
+        else:
+            if self.best_metric_stage2 is None or \
+                    self.cur_metric >= self.best_metric_stage2:
+                self.best_metric_stage2 = self.cur_metric
+
+    def before_save_checkpoint(self, runner: Runner, checkpoint: Dict) -> None:
+        assert self.cur_metric is not None, (
+            'cur_metric is None, please make sure to save checkpoint after '
+            'validation.')
+        assert self.best_epoch is not None, (
+            'best_epoch is None, please make sure to save checkpoint after '
+            'validation.')
+        assert self.best_metric is not None, (
+            'best_metric is None, please make sure to save checkpoint after '
+            'validation.')
+        assert self.best_epoch_stage1 is not None, (
+            'best_epoch_stage1 is None, please make sure to save checkpoint '
+            'after validation.')
+        assert self.best_metric_stage1 is not None, (
+            'best_metric_stage1 is None, please make sure to save checkpoint '
+            'after validation.')
+
+        if runner.epoch <= self.restart_epoch:
+            assert self.best_metric_stage2 is None, (
+                'best_metric_stage2 is not None in stage 1, please make sure '
+                'to save checkpoint after validation.')
+        else:
+            assert self.best_metric_stage2 is not None, (
+                'best_metric_stage2 is None in stage 2, please make sure to '
+                'save checkpoint after validation.')
+
+        checkpoint['ema_dynamic_momentum_dict'] = {
+            'restart_epoch': self.restart_epoch,
+            'interval': self.interval,
+            'metric': self.metric,
+            'cur_metric': self.cur_metric,
+            'best_epoch': self.best_epoch,
+            'best_metric': self.best_metric,
+            'best_epoch_stage1': self.best_epoch_stage1,
+            'best_metric_stage1': self.best_metric_stage1,
+            'best_metric_stage2': self.best_metric_stage2,
+            'momentum': self.ema_model.momentum,
+        }
+        super().before_save_checkpoint(runner, checkpoint)
+
+    def _load_ema_dynamic_momentum_dict(self, runner: Runner, checkpoint: Dict) -> None:
+        if 'ema_dynamic_momentum_dict' in checkpoint:
+            dynamic_momentum_dict = checkpoint['ema_dynamic_momentum_dict']
+            assert self.restart_epoch == dynamic_momentum_dict['restart_epoch'], (
+                f"Inconsistent restart_epoch: current={self.restart_epoch}, "
+                f"checkpoint={dynamic_momentum_dict['restart_epoch']}")
+            assert self.metric == dynamic_momentum_dict['metric'], (
+                f"Inconsistent metric: current={self.metric}, "
+                f"checkpoint={dynamic_momentum_dict['metric']}")
+
+            if self.interval != dynamic_momentum_dict['interval']:
+                print_log(
+                    f'Warning: interval changed: current={self.interval}, '
+                    f"checkpoint={dynamic_momentum_dict['interval']}",
+                    logger=runner.logger)
+            self.interval = dynamic_momentum_dict['interval']
+
+            assert dynamic_momentum_dict['cur_metric'] is not None, (
+                'cur_metric in the checkpoint is None, please make sure the '
+                'checkpoint is saved after validation.')
+            self.cur_metric = dynamic_momentum_dict['cur_metric']
+
+            assert dynamic_momentum_dict[
+                'best_epoch'] is not None, (
+                'best_epoch in the checkpoint is None, please make sure the '
+                'checkpoint is saved after validation.')
+            self.best_metric = dynamic_momentum_dict['best_metric']
+
+            assert self.best_metric is not None, (
+                'best_metric in the checkpoint is None, please make sure the '
+                'checkpoint is saved after validation.')
+            self.best_epoch = dynamic_momentum_dict['best_epoch']
+
+            assert dynamic_momentum_dict[
+                'best_epoch_stage1'] is not None, (
+                'best_epoch_stage1 in the checkpoint is None, please make sure '
+                'the checkpoint is saved after validation.')
+            self.best_epoch_stage1 = dynamic_momentum_dict['best_epoch_stage1']
+
+            assert dynamic_momentum_dict[
+                'best_metric_stage1'] is not None, (
+                'best_metric_stage1 in the checkpoint is None, please make sure '
+                'the checkpoint is saved after validation.')
+            self.best_metric_stage1 = dynamic_momentum_dict[
+                'best_metric_stage1']
+
+            self.best_metric_stage2 = dynamic_momentum_dict[
+                'best_metric_stage2']
+            self.ema_model.momentum = dynamic_momentum_dict['momentum']
+
+            print_log(
+                f'Resuming EMA dynamic momentum with: '
+                f'restart_epoch={self.restart_epoch}, '
+                f'interval={self.interval}, metric={self.metric}, '
+                f'cur_metric={self.cur_metric}, '
+                f'best_epoch={self.best_epoch}, '
+                f'best_metric={self.best_metric}, '
+                f'best_epoch_stage1={self.best_epoch_stage1}, '
+                f'best_metric_stage1={self.best_metric_stage1}, '
+                f'best_metric_stage2={self.best_metric_stage2}, '
+                f'momentum={self.ema_model.momentum}',
+                logger=runner.logger)
+
+    def after_load_checkpoint(self, runner: Runner, checkpoint: Dict) -> None:
+        self._load_ema_dynamic_momentum_dict(runner, checkpoint)
+        super().after_load_checkpoint(runner, checkpoint)
